4c7e457d6c86dbf79cb96a8d94deace7d235c2f2
Cassius <cassiusmaropene@gmail.com>
2025-05-13 23:26:29 +0200

    hipe

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT ;
 
 // Connect to MongoDB
 connectDB();
