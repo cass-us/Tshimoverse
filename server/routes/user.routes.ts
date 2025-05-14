b98d40f828ca63ba202f87f27dd92666ae9d92e3
Cassius <cassiusmaropene@gmail.com>
2025-05-14 09:15:12 +0200

    hello

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -3,9 +3,6 @@ import { registerUser } from '../controllers/auth.controller';
 
 const router = express.Router();
 
-// @route   POST /api/users/register
-// @desc    Register a new user
-// @access  Public
 router.post('/register', registerUser);
 
 export default router;
